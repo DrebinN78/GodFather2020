74ae31d5f6efa3ca0feadf5ba69301e5b36bb782
Dorian <dorian.gelas@gmail.com>
2020-09-02 12:48:04 +0200

    Menu Wireframe

---
+++ GamejamGodfather2020Gr3/Assets/Resources/Scripts/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public class MenuScript : MonoBehaviour
+{
+   public void LaunchGame()
+   {
+       SceneManager.LoadScene(1);
+   }
+
+   public void DisplayOptions()
+   {
+       
+   }
+
+   public void QuitGame()
+   {
+       Application.QuitGame();
+   }
+}
